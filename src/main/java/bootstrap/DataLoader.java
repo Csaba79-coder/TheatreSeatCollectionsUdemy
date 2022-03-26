3d65e2f654f1f25361654795e8001ceb1322fb86
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-03-26 17:03:21 +0100

    refactor sort method!

--- src/main/java/bootstrap/DataLoader.java
+++ src/main/java/bootstrap/DataLoader.java
@@ -133,7 +133,7 @@ public class DataLoader {
         List<Theatre.Seat> priceSeatsList = new ArrayList<>();
         priceSeatsList.add(theatre.new Seat("B00", 13.00));
         priceSeatsList.add(theatre.new Seat("A00", 13.00));
-        Collections.sort(priceSeatsList, Theatre.PRICE_ORDER);
+        priceSeatsList.sort(Theatre.PRICE_ORDER);
         Theatre.Seat.printList(priceSeatsList);
 
         List<Theatre.Seat> anotherFullPriceList = new ArrayList<>(theatre.getSeats());
